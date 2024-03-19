e41f4b07ea675f7387f0c876d982b8c7963e7a2a
Manjunath CR <manjunathcr@Manjunaths-MBP.lan>
2024-03-19 11:22:59 -0400

    js config update

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,15 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
+// import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
+import {babel} from '@rollup/plugin-babel';
 
 const packageJson = require("./package.json");
 
 export default [
   {
-    input: "src/index.ts",
+    input: "src/index.js",
     output: [
       {
         file: packageJson.main,
@@ -23,14 +24,20 @@ export default [
     ],
     plugins: [
       resolve(),
-      commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
       postcss(),
+      commonjs(),
+      // typescript({ tsconfig: "./tsconfig.json" }),
+     
+      babel({ 
+        exclude: 'node_modules/**',
+        presets: ['@babel/env', '@babel/preset-react']
+    }),
+    
     ],
   },
   {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    input: "dist/esm/index.js",
+    output: [{ file: "dist/index.js", format: "esm" }],
     plugins: [dts()],
     external: [/\.(css|less|scss)$/],
   },
